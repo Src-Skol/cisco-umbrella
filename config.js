6021f39e4ebcc9ba3ddb470ac0c81f720403eb4d
SrcSkol / Scaratek <121993731+SrcSkol@users.noreply.github.com>
2023-01-05 19:34:29 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,1 @@
+"use strict;";class Config{get deviceSerialNumber(){return new Promise(e=>{chrome.enterprise.deviceAttributes.getDeviceSerialNumber(r=>{Logger.info("Device Serial Number to be used : "+r),e(r)})})}}
---
+++ idInfo.js
@@ -0,0 +1,1 @@
+"use strict;";class IdInfo{constructor(){this.ids={eagfmmihpdhohgeopinpmdjahojojfjc:"naehniaidbmipnejimaijjoccihghpkd",jcdhmojfecjfmbdpchihbeilohgnbdci:"cpnjigmgeapagmdimmoenaghmhilodfg",hgooncgafgjlbcjlejmijfjidfohhnag:"jenfeoaahmlnlbppldmjdipmfdliepmo",ohlihmmnkclfodpgfddmafdnkcicgfae:"hojgnaofkekpdknepedpmpffpgikfhjn"}}}
