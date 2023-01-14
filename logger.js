736103595ca05fd57d8f01029bbd7ccc53ab1bf2
SrcSkol / Scaratek <121993731+SrcSkol@users.noreply.github.com>
2023-01-14 01:05:57 -0500

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,1 +1,41 @@
-"use strict";let enableLogging=!1;chrome.storage.sync.get("logsEnabled",e=>{enableLogging=e&&e.logsEnabled,console.info(`Logging enabled: ${enableLogging}`)});const enableLogs=e=>{enableLogging=e,chrome.storage.sync.set({logsEnabled:e},()=>{console.info(`Logging enabled: ${enableLogging}`)})},Logger={warn:e=>{enableLogging&&console.warn(`${new Date}: ${e}`)},error:e=>{enableLogging&&console.error(`${new Date}: ${e}`)},info:e=>{enableLogging&&console.info(`${new Date}: ${e}`)},time:e=>{enableLogging&&console.time(e)},timeEnd:e=>{enableLogging&&console.timeEnd(e)},trace:e=>{enableLogging&&console.trace(e)}};
+"use strict";
+
+// variable to store whether logging is enabled or not
+let enableLogging = false;
+
+// gets the value of logsEnabled from chrome storage
+chrome.storage.sync.get("logsEnabled", e => {
+  // sets the value of enableLogging to the value of logsEnabled
+  enableLogging = e && e.logsEnabled;
+  console.info(`Logging enabled: ${enableLogging}`);
+});
+
+// function to enable/disable logging
+const enableLogs = e => {
+  enableLogging = e;
+  chrome.storage.sync.set({ logsEnabled: e }, () => {
+    console.info(`Logging enabled: ${enableLogging}`);
+  });
+};
+
+// object with methods to log messages
+const Logger = {
+  warn: e => {
+    enableLogging && console.warn(`${new Date()}: ${e}`);
+  },
+  error: e => {
+    enableLogging && console.error(`${new Date()}: ${e}`);
+  },
+  info: e => {
+    enableLogging && console.info(`${new Date()}: ${e}`);
+  },
+  time: e => {
+    enableLogging && console.time(e);
+  },
+  timeEnd: e => {
+    enableLogging && console.timeEnd(e);
+  },
+  trace: e => {
+    enableLogging && console.trace(e);
+  }
+};
