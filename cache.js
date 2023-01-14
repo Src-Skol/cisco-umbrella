a21afa5505ac524b6c34f671ae5f140cffe8529f
SrcSkol / Scaratek <121993731+SrcSkol@users.noreply.github.com>
2023-01-14 01:02:49 -0500

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,1 +1,42 @@
-"use strict";class Cache{constructor(){let e=new Map;this.get=(t=>s=>{const o=s.domainName;let r=e.has(o)&&e.get(o);if(r&&Date.now()-r.timestamp<=3e5)Logger.info(`Cache hit for ${o}`);else{Logger.info(`Cache missed for ${o}`);try{const i=t(s);r={response:i,timestamp:Date.now()},i&&e.size<2500&&"debug"!==o&&e.set(o,r)}catch(e){return Logger.error(`UCC fail open for ${o} due to ${e.message}`),{cancel:!1}}}return r.response}),this.evictCache=(()=>{const t=e.size;for(let[t,s]of e)Date.now()-s.timestamp>3e5&&e.delete(t);Logger.info(`Evicted ${t-e.size} expired cache entries.`)})}}
+"use strict";
+
+class Cache {
+  constructor() {
+    // creates a new map object
+    let e = new Map;
+    // assigns the get method to the class
+    this.get = (t => s => {
+      // assigns the domainName variable
+      const o = s.domainName;
+      // checks if the domainName is in the cache and if it's timestamp is less than or equal to 3e5
+      let r = e.has(o) && e.get(o);
+      if (r && Date.now() - r.timestamp <= 3e5) {
+        Logger.info(`Cache hit for ${o}`);
+      } else {
+        Logger.info(`Cache missed for ${o}`);
+        try {
+          // assigns the response and timestamp variables
+          const i = t(s);
+          r = {
+            response: i,
+            timestamp: Date.now()
+          }, i && e.size < 2500 && "debug" !== o && e.set(o, r);
+        } catch (e) {
+          return Logger.error(`UCC fail open for ${o} due to ${e.message}`), {
+            cancel: !1
+          }
+        }
+      }
+      // returns the response variable
+      return r.response;
+    }), this.evictCache = (() => {
+      // assigns the size variable
+      const t = e.size;
+      // iterates through the map
+      for (let [t, s] of e)
+        // checks if the timestamp is greater than 3e5
+        Date.now() - s.timestamp > 3e5 && e.delete(t);
+      Logger.info(`Evicted ${t - e.size} expired cache entries.`)
+    });
+  }
+}
