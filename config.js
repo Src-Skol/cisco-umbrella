3b396af57520d5ad18baec9c5e4d2a9125de56d5
SrcSkol / Scaratek <121993731+SrcSkol@users.noreply.github.com>
2023-01-14 01:03:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
-"use strict;";class Config{get deviceSerialNumber(){return new Promise(e=>{chrome.enterprise.deviceAttributes.getDeviceSerialNumber(r=>{Logger.info("Device Serial Number to be used : "+r),e(r)})})}}
+"use strict;";
+
+class Config {
+  // getter function for deviceSerialNumber
+  get deviceSerialNumber() {
+    return new Promise(e => {
+      chrome.enterprise.deviceAttributes.getDeviceSerialNumber(r => {
+        Logger.info("Device Serial Number to be used : " + r);
+        e(r);
+      });
+    });
+  }
+}
