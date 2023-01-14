ec9fa6f54afbc08d122b5325016f607006814695
SrcSkol / Scaratek <121993731+SrcSkol@users.noreply.github.com>
2023-01-14 01:04:53 -0500

    Update idInfo.js

--- idInfo.js
+++ idInfo.js
@@ -1,1 +1,13 @@
-"use strict;";class IdInfo{constructor(){this.ids={eagfmmihpdhohgeopinpmdjahojojfjc:"naehniaidbmipnejimaijjoccihghpkd",jcdhmojfecjfmbdpchihbeilohgnbdci:"cpnjigmgeapagmdimmoenaghmhilodfg",hgooncgafgjlbcjlejmijfjidfohhnag:"jenfeoaahmlnlbppldmjdipmfdliepmo",ohlihmmnkclfodpgfddmafdnkcicgfae:"hojgnaofkekpdknepedpmpffpgikfhjn"}}}
+"use strict";
+
+class IdInfo {
+  constructor() {
+    // creates an object with keys as values and values as keys
+    this.ids = {
+      eagfmmihpdhohgeopinpmdjahojojfjc: "naehniaidbmipnejimaijjoccihghpkd",
+      jcdhmojfecjfmbdpchihbeilohgnbdci: "cpnjigmgeapagmdimmoenaghmhilodfg",
+      hgooncgafgjlbcjlejmijfjidfohhnag: "jenfeoaahmlnlbppldmjdipmfdliepmo",
+      ohlihmmnkclfodpgfddmafdnkcicgfae: "hojgnaofkekpdknepedpmpffpgikfhjn"
+    };
+  }
+}
